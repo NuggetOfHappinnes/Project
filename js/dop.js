c499b24fc2f168a38f9b63a63d89f6bc222a7d12
Tesla09 <99958195+NuggetOfHappinnes@users.noreply.github.com>
2022-05-07 23:21:59 +0300

    Update dop.js

--- js/dop.js
+++ js/dop.js
@@ -1,10 +1,13 @@
 (function () {
     const qr = document.querySelector('.btn'); 
     const menu = document.querySelector('.qr');        
-
+    const menuCloseItem = document.querySelector('.btn1');
     qr.addEventListener('click', () => {
         menu.classList.add('qr__active');      
 
-    });        
+    });    
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('qr__active');
+    });    
 }());
 
